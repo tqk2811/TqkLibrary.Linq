5fd045e651c5fd00f74d5d29c7cfbb24e32cca56
tqk2811 <tqk2811@gmail.com>
2023-07-22 18:11:01 +0700

    add Combinations & Permutations

---
+++ TqkLibrary.Linq/Extensions.CombinationsPermutations.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace TqkLibrary.Linq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="elements"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Combinations<T>(this IEnumerable<T> elements, int k)
+        {
+            //https://stackoverflow.com/a/33336576/5034139
+            if (k <= 0) return new[] { new T[0] };
+            return elements
+                .SelectMany((e, i) => elements.Skip(i + 1)
+                                            .Combinations(k - 1)
+                                            .Select(c => c.Append(e)));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="elements"></param>
+        /// <param name="k"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Permutations<T>(this IEnumerable<T> elements, int k)
+        {
+            foreach (var item in elements.Combinations(k))
+            {
+                foreach (var item2 in permutate(item, Enumerable.Empty<T>()))
+                {
+                    yield return item2;
+                }
+            }
+            IEnumerable<IEnumerable<T>> permutate(IEnumerable<T> reminder, IEnumerable<T> prefix)
+            {
+                if (reminder.Any())
+                {
+                    return reminder.SelectMany((c, i) => permutate(reminder.Take(i).Concat(reminder.Skip(i + 1)), prefix.Append(c)));
+                }
+                else
+                {
+                    return Enumerable.Repeat(prefix, 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Permutations<T>(this IEnumerable<T> elements)
+            => elements.Permutations(elements.Count());
+    }
+}

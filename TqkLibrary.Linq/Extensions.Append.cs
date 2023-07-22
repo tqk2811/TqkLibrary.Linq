ec7458db557fc9af6c669a801e8ad69f805d28b9
tqk2811 <tqk2811@gmail.com>
2023-07-22 17:54:03 +0700

    add Append

---
+++ TqkLibrary.Linq/Extensions.Append.cs
@@ -0,0 +1,22 @@
+﻿namespace TqkLibrary.Linq
+{
+    public static partial class Extensions
+    {
+#if !NET5_0_OR_GREATER
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sources"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> sources, params T[] values)
+        {
+            foreach (var item in sources)
+                yield return item;
+            foreach (var item in values)
+                yield return item;
+        }
+#endif
+    }
+}

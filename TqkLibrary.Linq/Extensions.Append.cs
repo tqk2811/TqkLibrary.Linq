b7aac89ac8c9a47af9e5514006c646be933e2bb8
tqk2811 <tqk2811@gmail.com>
2023-08-23 10:36:01 +0700

    Append update

--- TqkLibrary.Linq/Extensions.Append.cs
+++ TqkLibrary.Linq/Extensions.Append.cs
@@ -18,5 +18,48 @@
                 yield return item;
         }
 #endif
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sources"></param>
+        /// <param name="find"></param>
+        /// <param name="append"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> sources, Predicate<T> find, Func<T, T> append)
+        {
+            if (find is null) throw new ArgumentNullException(nameof(find));
+            if (append is null) throw new ArgumentNullException(nameof(append));
+            foreach (var item in sources)
+            {
+                yield return item;
+                if (find(item))
+                {
+                    yield return append(item);
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sources"></param>
+        /// <param name="find"></param>
+        /// <param name="append"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> sources, Predicate<T> find, Func<T> append)
+        {
+            if (find is null) throw new ArgumentNullException(nameof(find));
+            if (append is null) throw new ArgumentNullException(nameof(append));
+            foreach (var item in sources)
+            {
+                yield return item;
+                if (find(item))
+                {
+                    yield return append();
+                }
+            }
+        }
     }
 }

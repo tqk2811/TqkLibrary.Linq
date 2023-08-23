bd2a280bb25d6c1897b2ec8e4c005e182e3dd475
tqk2811 <tqk2811@gmail.com>
2023-08-23 10:32:47 +0700

    add Replace

---
+++ TqkLibrary.Linq/Extensions.Replace.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TqkLibrary.Linq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sources"></param>
+        /// <param name="predicate"></param>
+        /// <param name="replace"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> Replace<T>(this IEnumerable<T> sources, Predicate<T> predicate, Func<T> replace)
+        {
+            if (sources is null) throw new ArgumentNullException(nameof(sources));
+            if (predicate is null) throw new ArgumentNullException(nameof(predicate));
+            if (replace is null) throw new ArgumentNullException(nameof(replace));
+            foreach (var item in sources)
+            {
+                if(predicate(item))
+                {
+                    yield return replace();
+                }
+                else
+                {
+                    yield return item;
+                }
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="sources"></param>
+        /// <param name="replace"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> Replace<T>(this IEnumerable<T> sources, Func<T,T> replace)
+        {
+            if (sources is null) throw new ArgumentNullException(nameof(sources));
+            if (replace is null) throw new ArgumentNullException(nameof(replace));
+            foreach (var item in sources)
+            {
+                yield return replace(item);
+            }
+        }
+    }
+}

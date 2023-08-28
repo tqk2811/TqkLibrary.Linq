4ef241e018b7827d5f6b502a1014e1aef0ac737c
tqk2811 <tqk2811@gmail.com>
2023-08-28 11:14:24 +0700

    add Paginated

---
+++ TqkLibrary.Linq/Extensions.Paginated.cs
@@ -0,0 +1,22 @@
+﻿namespace TqkLibrary.Linq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="xs"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Paginated<T>(this IEnumerable<T> xs, int pageSize) =>
+            Enumerable.Range(0, (int)Math.Ceiling(decimal.Divide(xs.Count(), pageSize)))
+              .Select(i =>
+                xs
+                  .Skip(i * pageSize)
+                  .Take(pageSize));
+    }
+}

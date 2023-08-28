88ac3f384d1ef7df2e557474c7a5a13a8142deb2
tqk2811 <tqk2811@gmail.com>
2023-08-28 11:13:49 +0700

    add PageIterator

---
+++ TqkLibrary.Linq/Extensions.PageIterator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+
+namespace TqkLibrary.Linq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> PageIterator<T>(this IEnumerable<T> source, int pageSize)
+        {
+            Contract.Requires(source != null);
+            Contract.Requires(pageSize > 0);
+            Contract.Ensures(Contract.Result<IEnumerable<IEnumerable<T>>>() != null);
+
+            using (var enumerator = source.GetEnumerator())
+            {
+                while (enumerator.MoveNext())
+                {
+                    var currentPage = new List<T>(pageSize)
+                    {
+                        enumerator.Current
+                    };
+
+                    while (currentPage.Count < pageSize && enumerator.MoveNext())
+                    {
+                        currentPage.Add(enumerator.Current);
+                    }
+                    yield return new ReadOnlyCollection<T>(currentPage);
+                }
+            }
+        }
+    }
+}

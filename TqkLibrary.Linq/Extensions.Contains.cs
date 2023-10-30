65a78f7d270bff18bc3e4c4dc71ef141e328c1a7
tqk2811 <tqk2811@gmail.com>
2023-10-30 10:20:35 +0700

    add Contains

---
+++ TqkLibrary.Linq/Extensions.Contains.cs
@@ -0,0 +1,33 @@
+﻿namespace TqkLibrary.Linq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="items"></param>
+        /// <param name="items2"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static bool Contains<T>(this IEnumerable<T> items, IEnumerable<T> items2)
+        {
+            if (items is null)
+                throw new ArgumentNullException(nameof(items));
+            if (items2 is null)
+                throw new ArgumentNullException(nameof(items2));
+
+            foreach (var item in items)
+            {
+                if (items2.Contains(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}

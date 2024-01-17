6693d12ce3f1662ff0508f1d19d40c73b2ced69c
tqk2811 <tqk2811@gmail.com>
2024-01-17 14:38:06 +0700

    add SumTimeSpan

---
+++ TqkLibrary.Linq/Extensions.SumTimeSpan.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Linq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="timeSpans"></param>
+        /// <returns></returns>
+        public static TimeSpan Sum(this IEnumerable<TimeSpan> timeSpans)
+        {
+            TimeSpan timeSpan = TimeSpan.Zero;
+            foreach (var item in timeSpans)
+            {
+                timeSpan += item;
+            }
+            return timeSpan;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collections"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static TimeSpan Sum<T>(this IEnumerable<T> collections, Func<T, TimeSpan> func)
+        {
+            TimeSpan timeSpan = TimeSpan.Zero;
+            foreach (var item in collections)
+            {
+                timeSpan += func.Invoke(item);
+            }
+            return timeSpan;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="collections"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static async ValueTask<TimeSpan> SumAsync<T>(this IEnumerable<T> collections, Func<T, ValueTask<TimeSpan>> func)
+        {
+            TimeSpan timeSpan = TimeSpan.Zero;
+            foreach (var item in collections)
+            {
+                timeSpan += await func.Invoke(item);
+            }
+            return timeSpan;
+        }
+    }
+}

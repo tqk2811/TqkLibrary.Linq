1809c0d3a15d8629475028fa436b8779f9f5d0c8
tqk2811 <tqk2811@gmail.com>
2024-01-17 14:28:56 +0700

    add ForEachAsync

--- TqkLibrary.Linq/Extensions.ForEach.cs
+++ TqkLibrary.Linq/Extensions.ForEach.cs
@@ -1,4 +1,7 @@
-﻿namespace TqkLibrary.Linq
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Linq
 {
     public static partial class Extensions
     {
@@ -20,6 +23,20 @@
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="source"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, Task> func)
+        {
+            foreach (var item in source)
+            {
+                await func.Invoke(item);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
         /// <param name="action"></param>
         public static void ForEach<T>(this IEnumerable<T> source, Action<T, int> action)
         {
@@ -27,6 +44,21 @@
             foreach (var item in source)
             {
                 action.Invoke(item, index++);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="func"></param>
+        /// <returns></returns>
+        public static async Task ForEachAsync<T>(this IEnumerable<T> source, Func<T, int, Task> func)
+        {
+            int index = 0;
+            foreach (var item in source)
+            {
+                await func.Invoke(item, index++);
             }
         }
     }

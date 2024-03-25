d19b0e248da8fad4e878d5faa41d064cc00f17dc
tqk2811 <tqk2811@gmail.com>
2024-03-25 15:00:40 +0700

    update GetRandomItem

--- TqkLibrary.Linq/Extensions.GetRandomItem.cs
+++ TqkLibrary.Linq/Extensions.GetRandomItem.cs
@@ -11,8 +11,10 @@ namespace TqkLibrary.Linq
         /// <param name="sources"></param>
         /// <param name="seed"></param>
         /// <returns></returns>
-        public static T GetRandomItem<T>(this IEnumerable<T> sources, int seed)
-            => sources.OrderByRandom(seed).FirstOrDefault();
+        public static T? GetRandomItem<T>(this IEnumerable<T> sources, int seed)
+        {
+            return sources.GetRandomItem(new Random(seed));
+        }
         /// <summary>
         /// 
         /// </summary>
@@ -20,8 +22,12 @@ namespace TqkLibrary.Linq
         /// <param name="sources"></param>
         /// <param name="random"></param>
         /// <returns></returns>
-        public static T GetRandomItem<T>(this IEnumerable<T> sources, Random? random = null)
-            => sources.OrderByRandom(random).FirstOrDefault();
+        public static T? GetRandomItem<T>(this IEnumerable<T> sources, Random? random = null)
+        {
+            if (random is null)
+                random = new Random(unchecked(sources.GetHashCode() + DateTime.Now.Millisecond));
+            return sources.At(random.Next(sources.Count()));
+        }
 
 
         /// <summary>

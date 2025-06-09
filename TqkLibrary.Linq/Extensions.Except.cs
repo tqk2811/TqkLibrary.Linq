85a88472617be85899b745f5066721ccd5dc56f4
tqk2811 <tqk2811@gmail.com>
2025-06-09 15:31:25 +0700

    add ExceptWithField

--- TqkLibrary.Linq/Extensions.Except.cs
+++ TqkLibrary.Linq/Extensions.Except.cs
@@ -11,31 +11,5 @@
         /// <returns></returns>
         public static IEnumerable<T> Except<T>(this IEnumerable<T> source, params T[] values)
             => source.Except(values.AsEnumerable());
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <typeparam name="TField"></typeparam>
-        /// <param name="source"></param>
-        /// <param name="fieldSelector"></param>
-        /// <param name="values"></param>
-        /// <returns></returns>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static IEnumerable<T> Except<T, TField>(this IEnumerable<T> source, Func<T, TField> fieldSelector, params TField[] values)
-        {
-            if (source == null) throw new ArgumentNullException(nameof(source));
-            if (fieldSelector == null) throw new ArgumentNullException(nameof(fieldSelector));
-            if (values == null) throw new ArgumentNullException(nameof(values));
-
-            var valueSet = new HashSet<TField>(values);
-            foreach (var item in source)
-            {
-                if (!valueSet.Contains(fieldSelector(item)))
-                {
-                    yield return item;
-                }
-            }
-        }
     }
 }
---
+++ TqkLibrary.Linq/Extensions.ExceptWithField.cs
@@ -0,0 +1,42 @@
+﻿namespace TqkLibrary.Linq
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TField"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="fieldSelector"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> ExceptWithField<T, TField>(this IEnumerable<T> source, Func<T, TField> fieldSelector, params TField[] values)
+            => ExceptWithField(source, fieldSelector, values.AsEnumerable());
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TField"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="fieldSelector"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static IEnumerable<T> ExceptWithField<T, TField>(this IEnumerable<T> source, Func<T, TField> fieldSelector, IEnumerable<TField> values)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (fieldSelector == null) throw new ArgumentNullException(nameof(fieldSelector));
+            if (values == null) throw new ArgumentNullException(nameof(values));
+
+            var valueSet = new HashSet<TField>(values);
+            foreach (var item in source)
+            {
+                if (!valueSet.Contains(fieldSelector(item)))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}

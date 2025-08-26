c7d93b1948bca6d8015cdd25a672e671d3660fef
tqk2811 <tqk2811@gmail.com>
2025-08-26 17:05:40 +0700

    add Transpose

---
+++ TqkLibrary.Linq/Extensions.transpose.cs
@@ -0,0 +1,19 @@
+﻿namespace TqkLibrary.Linq
+{
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        public static IEnumerable<IEnumerable<T>> Transpose<T>(this IEnumerable<IEnumerable<T>> source)
+        {
+            return source
+                .SelectMany(inner => inner.Select((value, colIndex) => new { colIndex, value }))
+                .GroupBy(x => x.colIndex)
+                .Select(g => g.Select(x => x.value));
+        }
+    }
+}

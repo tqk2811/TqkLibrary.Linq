b770c81f133c4c02ee54b8ab03e0b7f561d4d065
tqk2811 <tqk2811@gmail.com>
2025-12-04 17:28:51 +0700

    add SellectAsync

---
+++ TqkLibrary.Linq/Extensions.SellectAsync.cs
@@ -0,0 +1,86 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Linq
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static partial class Extensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TSelector"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static async Task<IReadOnlyList<TSelector>> SellectAsync<T, TSelector>(
+            this IEnumerable<T> source,
+            Func<T, Task<TSelector>> selector
+            )
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (selector is null) throw new ArgumentNullException(nameof(selector));
+            List<TSelector> results = new();
+            foreach (var item in source)
+            {
+                results.Add(await selector(item));
+            }
+            return results;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TSelector"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="selector"></param>
+        /// <param name="maxDegreeOfParallelism">0 is run all same time</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentOutOfRangeException"></exception>
+        public static async Task<IReadOnlyList<TSelector>> SellectAsync<T, TSelector>(
+            this IEnumerable<T> source,
+            Func<T, Task<TSelector>> selector,
+            int maxDegreeOfParallelism
+            )
+        {
+            if (source is null) throw new ArgumentNullException(nameof(source));
+            if (selector is null) throw new ArgumentNullException(nameof(selector));
+
+            var tasks = new List<Task<TSelector>>();
+            if (maxDegreeOfParallelism <= 0)
+            {
+                foreach (var item in source)
+                {
+                    tasks.Add(Task.Run(async () => await selector(item)));
+                }
+                return await Task.WhenAll(tasks);
+            }
+            else
+            {
+                using var sem = new SemaphoreSlim(maxDegreeOfParallelism);
+                foreach (var item in source)
+                {
+                    await sem.WaitAsync();
+
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        try
+                        {
+                            return await selector(item);
+                        }
+                        finally
+                        {
+                            sem.Release();
+                        }
+                    }));
+                }
+                return await Task.WhenAll(tasks);
+            }
+        }
+    }
+}
